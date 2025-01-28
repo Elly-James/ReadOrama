8f486ef1c65e5c2356ea40b370974b6ae43fbbdc
Elly <ellyjames1999@gmail.com>
2025-01-28 18:16:31 +0300

    done

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,11 +3,15 @@ import React from 'react';
 import SideBar from './SideBar';
 
 import Shop from './Shop';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
       <SideBar />
+
+
+      <Home/>
       <Shop />
     </div>
   );
---
+++ src/Components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function  Home () {
+
+    return (
+        <div className='home'>
+            <h1>Welcome to ReadOrama </h1>
+            <p>Your One Stop Shop For All Your Books Solutions</p>
+            <button>Shop Now</button>
+            
+        </div>
+    )
+
+}
+
+
+export default Home
