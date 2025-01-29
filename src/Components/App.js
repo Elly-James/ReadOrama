47937e45e5cc5751a33dbd5d9fa2b78803a77dd6
DanHacks <hydancheru@gmail.com>
2025-01-29 21:58:38 +0300

    Shop js

--- src/Components/App.js
+++ src/Components/App.js
@@ -5,16 +5,16 @@ import SideBar from './SideBar';
 import Shop from './Shop';
 import Home from './Home';
 
-function App() {
+function Filter({ onFilter, onSort}) {
   return (
-    <div className="App">
-      <SideBar />
-
-
-      <Home/>
-      <Shop />
+    <div className="filter">
+      <button onClick={() => onFilter("All")}>All Books</button>
+      <button onClick={() => onFilter("Computer")}>Computers Books</button>
+      <button onClick={() => onFilter("History")}>History Books</button>
+      <button onClick={() => onSort("asc")}>Sort by Price (Low to High)</button>
+      <button onClick={() => onSort("desc")}>Sort by Price (High to Low)</button>
     </div>
   );
 }
 
-export default App;
+export default Filter;
--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Filter from './Filter';
 
 function Shop () {
+    const [books, setBooks] = useState([]);
+    const [filteredBooks, setFilteredBooks] = useState([]);
+    //fetch books from db.json
+    useEffect(() => {
+        fetch('http://localhost:4000/books')
+           .then(response => response.json())
+           .then(data => setBooks(data))
+           .catch(error => console.error('Error:', error));
+    }, []);
+
+    const [categories, setCategories] = useState('All');
+
+    const handleCategoriesChange = (category) => {
+        setCategories(category);
+        if (category === 'All') {
+            setFilteredBooks(books);
+        } else {
+            setFilteredBooks(books.filter(book => book.category === category));
+        }
+    };
+
     return (
         <div>
             <h2>Shop</h2>
-            <Filter />
+            <filter onFilter={filterBooks} onSort={sortBooks} />
+            <div id="book-list">
+                {filteredBooks.map(book => (
+                    <div key={book.id}>
+                        <h3>{book.title}</h3>
+                        <p>{book.author}</p>
+                        <p>{book.price}</p>
+                    </div>
+                ))}
+            </div>
         </div>
-    )
-}
-
+    );
 export default Shop;
