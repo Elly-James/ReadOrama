0e2f10f61690b192e4661d3ec7f616f8797c58b6
Elly <ellyjames1999@gmail.com>
2025-02-03 08:30:44 +0300

    dH

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,9 +7,11 @@ function  Home () {
 
     return (
         <div className='home'>
-            <h1 className='page'>Welcome to ReadOrama </h1>
+            <h1 className='page'>Welcome to ReadOrama</h1>
             <p className='page'>Your One Stop Shop For All Your Books Solutions</p>
-            <button>Shop Now</button>
+            <img  className= "" src=''/>
+            <a href= "/shop">  <button>Shop Now</button></a>
+          
             
         </div>
     )
