13c163c2427ed1cfb65cfef864e09253177ab707
Elly James <ellyjames1999@gmail.com>
2026-03-13 02:33:25 +0300

    Add missing homeBg import in Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,22 +1,14 @@
 import React from 'react';
+import homeBg from '../home.png';
 
-function  Home () {
-
-    
-
-
-    return (
-        <div className='home' style={{ backgroundImage: `url(${homeBg})` }}>
-            <h1 className='page'>Welcome to ReadOrama</h1>
-            <p className='page'>Your One Stop Shop For All Your Books Solutions</p>
-           
-            <a href= "/shop">  <button>Shop Now</button></a>
-          
-            
-        </div>
-    )
-
+function Home() {
+  return (
+    <div className='home' style={{ backgroundImage: `url(${homeBg})` }}>
+      <h1 className='page'>Welcome to ReadOrama</h1>
+      <p className='page'>Your One Stop Shop For All Your Books Solutions</p>
+      <a href="/shop"><button>Shop Now</button></a>
+    </div>
+  );
 }
 
-
-export default Home
+export default Home;
